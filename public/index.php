f6672a3ce58f6c50e53c16cee1ed3ae52fa3daec
mrmatteorusso <mrmatteorusso@gmail.com>
2022-10-01 18:53:05 +0200

    adding header in index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, PUT, OPTIONS');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 use App\Controllers\ProductController;
+
 use App\Routing\Router;
 
 require_once __DIR__ . '/../vendor/autoload.php';
