7273cd9b8c3ca9b70a55aa5cf699a5715e0ac171
mrmatteorusso <mrmatteorusso@gmail.com>
2022-10-01 18:01:00 +0200

    database new config

--- app/Database/Connection.php
+++ app/Database/Connection.php
@@ -11,10 +11,14 @@ class Connection
 
     public function connect()
     {
-        $user = 'root';
-        $password = 'password';
-        $host = 'mysql';
-        $dbName = "scandiweb";
+        // $user = 'root';
+        // $password = 'password';
+        // $host = 'mysql';
+        // $dbName = "scandiweb";
+        $user = 'gssdvq1oz8bvlpvy';
+        $password = 'y6r57fswz6owh5op';
+        $host = 'ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+        $dbName = 'p5t3d4ceejfhd8q6';
 
         $this->database = new PDO("mysql:host=$host;dbname=$dbName;", $user, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
